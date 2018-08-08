8dae5f879ac0fc3427f1230df129d8cd50f766f1
Igor Moura <imp2@cin.ufpe.br>
2018-08-08 17:06:33 -0300

    WIP

--- CMACLib.h
+++ CMACLib.h
@@ -1,11 +1,18 @@
 #ifndef CMACLIB_H
 #define CMACLIB_H
 #include <stdint.h>
+#include "AESLib.h"
 #ifdef __cplusplus
 extern "C"{
 #endif
 
+// Add struct for keys (K1 & K2)
 
+// Add function to generate subkeys
+
+// Add function to generate MAC 
+
+// Add function to verify MAC for a message
 
 
 #ifdef __cplusplus

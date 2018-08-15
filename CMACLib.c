71c6f7ba5e6577c34a93ed7656738808d3b83c6b
Igor Moura <imp2@cin.ufpe.br>
2018-08-15 17:05:20 -0300

    WIP on subkey generation

--- CMACLib.c
+++ CMACLib.c
@@ -1,17 +1,61 @@
 #include "CMACLib.h"
 #include <stdint.h>
 
+// Returns if the most significant bit is 1 or 0 in a byte
+static inline bool cmac_msb(uint8_t x){
+    return (x & 128)?1:0;
+}
+
+// Left shifts a whole array by 1 in place
+static void cmac_lshift(uint8_t *in, uint8_t len) {
+    uint8_t carry, prev_carry = 0;  
+    for(int8_t i = (len-1); i >= 0 ; --i){
+        carry = cmac_msb(in[i]);
+        in[i] <<= 1;
+        in[i] += prev_carry;
+        prev_carry = carry;
+    }
+}
+
+// Xors 2 arrays byte per byte
+static void cmac_xor(uint8_t *x, uint8_t *y, uint8_t len){
+    for(int8_t i = 0; i < len; i++){
+        x[i] ^= y[i];
+    }
+}
 
 cmac_subkeys_t* cmac_generate_subkeys(uint8_t *key, uint8_t key_len = 16){
     cmac_subkeys_t subkey;
+
+    // Rb value according to section 5.3 of the NIST doc
+    const uint8_t Rb = 135;
+
+    // Does L = CIPHk(0b) 
     uint8_t L[key_len] = {0};
-    aes128_enc_multiple(key, L);
-    if(L[0] & 128){
-        // add shift for the whole key xor rb
+    aes128_enc_multiple(key, L, key_len);
+
+
+    if( (L[0] & 128) == 0 ){
+        cmac_lshift(L, 16);
+        subkey.sk1 = L;
     }
     else{
-        // add shift for the whole key
+        cmac_lshift(L, 16);
+        L[len-1] ^= Rb;
+        subkey.sk1 = L;
     }
+
+    if( (subkey.sk1[0] & 128) == 0 ){
+        cmac_lshift(subkey.sk1, 16);
+        subkey.sk2 = subkey.sk1;
+    }
+    else{
+        cmac_lshift(subkey.sk1, 16);
+        subkey.sk1[len-1] ^= Rb;
+        subkey.sk2 = subkey.sk1;
+    }
+
+    return subkey;
 
 }
 
